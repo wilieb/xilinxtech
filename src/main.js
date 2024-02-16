7b58ba62166b6abd31966b8f6ef04906776567b1
William Owalla <owallabilly@gmail.com>
2024-02-16 08:01:10 +0300

    dependencies set up

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// VueKonva
+import VueKonva from 'vue-konva';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+app.use(vuetify)
+app.use(VueKonva)
+app.mount('#app')

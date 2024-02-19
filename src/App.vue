cbadd6926142a6eba1eb12912d9479141f659de8
William Owalla <owallabilly@gmail.com>
2024-02-19 14:16:50 +0300

    rename components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from './components/HomeComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeComponent
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="12" md="8">
+                <div ref="stageContainer">
+                    Lets get it done with, already!
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import Konva from 'konva';
+
+export default {
+    mounted() {
+        this.initKonva();
+    },
+    methods: {
+        initKonva() {
+            const stage = new Konva.Stage({
+                container: this.$refs.stageContainer,
+                width: 500,
+                height: 500,
+            });
+
+            const layer = new Konva.Layer();
+            const circle = new Konva.Circle({
+                x: stage.width() / 2,
+                y: stage.height() / 2,
+                radius: 50,
+                fill: 'red',
+            });
+
+            layer.add(circle);
+            stage.add(layer);
+        },
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
